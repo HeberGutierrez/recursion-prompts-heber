c1f4dade5532e3b75900dd1a42ee34f3aaf1ba40
HeberGutierrez <hebergutierrez.p@gmail.com>
2018-01-08 13:16:52 -0600

    Integer Range solved

--- CODEACADEMY-RECURSION.js
+++ CODEACADEMY-RECURSION.js
@@ -113,3 +113,34 @@ function factorial(n) {
   return n * factorial(n - 1);
 
 }
+
+
+//COUNTING SHEEP
+
+// Here is the function using a for loop
+function countSheepWithLoop(number) {
+  for (i = 1; i <= number; i++) {
+    if (i === number) {
+      console.log("Zzzzzz");
+    }
+    console.log("Another sheep jumps over the fence.");
+  }
+}
+
+// And here is the recursive function
+function countSheep(number) {
+  if (number === 0) {
+    console.log("Zzzzzz");
+  } else {
+    console.log("Another sheep jumps over the fence.");
+    // Define the variable newNumber as
+    // 1 less than the input variable number
+    var newNumber = number - 1;
+    // Recursively call the function
+    // with newNumber as the parameter
+    countSheep(newNumber);
+  }
+}
+
+// Call your new recursive function:
+console.log("The recursive function returned: " + countSheep(5));
