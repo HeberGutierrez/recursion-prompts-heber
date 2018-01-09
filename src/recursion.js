719fd8fef3b8cb22d7b99cc5f1be2d5cad4ead91
HeberGutierrez <hebergutierrez.p@gmail.com>
2018-01-08 23:04:54 -0600

    Recursive map Solved

--- src/recursion.js
+++ src/recursion.js
@@ -282,11 +282,19 @@ var fizzBuzz = function(n) {
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
-var countOccurrence = function(array, value) {};
+var countOccurrence = function(array, value) {
+  if (!array.length) return 0;
+  return (array[0] === value) + countOccurrence(array.slice(1), value);
+};
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
-var rMap = function(array, callback) {};
+var rMap = function(array, callback) {
+  if (array.length === 0) {
+    return [];
+  }
+  return [callback(array[0])].concat(rMap(array.slice(1), callback));
+};
 
 // 22. Write a function that counts the number of times a key occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
