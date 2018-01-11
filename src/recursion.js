3bb556d12a42dcb4878b2c4f5e69b0b433eabb25
HeberGutierrez <hebergutierrez.p@gmail.com>
2018-01-10 22:30:11 -0600

    Return nth Fibonacci - Solved

--- src/recursion.js
+++ src/recursion.js
@@ -371,7 +371,15 @@ var fibonacci = function(n) {
 // nthFibo(5); // 5
 // nthFibo(7); // 13
 // nthFibo(3); // 2
-var nthFibo = function(n) {};
+var nthFibo = function(n) {
+  if (n < 0) {
+    return null
+  }
+  if (n === 0 || n === 1) {
+    return n
+  }
+  return nthFibo(n - 1) + nthFibo(n - 2);
+};
 
 // 27. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
